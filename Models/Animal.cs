e37e61c4b2ca9dc74e324a790b50b5273e547553
HoldenJC <holdenj_c@yahoo.com>
2019-08-20 17:43:37 -0700

    add full WildlifePark.Solution project that follows along with C# week 6 weekend HW

---
+++ Models/Animal.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WildlifePark.Models
+{
+  [Table("animals")]
+  public class Animal
+  {
+    [Key]
+    public int AnimalId { get; set; }
+    [Required]
+    public string Species { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Name { get; set; }
+    [Required]
+    [Range(0, 100, ErrorMessage = "Age must be between 0 and 100.")]
+    public int Age { get; set; }
+    [Required]
+    public string Gender { get; set; }
+  }
+}
